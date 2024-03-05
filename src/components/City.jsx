4ee72fbcaffe04bc16b8fd4326dae0d3bcb7418c
elmar <elmarcera@gmail.com>
2024-03-05 15:24:07 +0800

    Back to The WorldWise App - optimize

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -18,13 +18,64 @@ function City() {
   const { id } = useParams();
   const { getCity, currentCity, isLoading } = useCities();
 
+  ////////////////////
+  //   Now, let's just remember that there was some issue earlier.
+  // And so we are now actually ready to fix that.
+  // So if we come back here to our city,
+  // remember that in the use effect,
+  // we left out the getCity function.
+  // So here we see that issue where eslint is telling us
+  // to add the getCity, but remember that this created
+  // an infinite loop of HTTP requests to our API.
+
+  //   But first of all, of course, we need to understand
+  // what is actually happening.
+  // So now that we have this getCity function
+  // in our dependency array, the effect will rerun
+  // each time that it get city function gets updated,
+  // or in other words, that it gets recreated.
+  // Now, when does this function get recreated?
+  // Well, since it lives in the context, so let's open that.
+  // So this file, getCity, lives here,
+  // so it is created in this city's provider.
+  // But the problem is that this getCity function
+  // will update the state each time that it is executed,
+  // which will then end up in an infinite loop.
+  // So here we call the getCity function, right?
+  // And so that function will then update the state
+  // in this component.
+  // Then this component will re-render,
+  // which will then recreate this function.
+  // And as the function gets recreated,
+  // since it is here in the dependency array,
+  // then getCity will get called again,
+  // which then again will update a state, which will re-render,
+  // which will cause the effect to run over,
+  // and over, and over again.
+  // So that's the infinite loop that we have right here,
+  // and probably we should come back here
+  // and now take our application out of this misery.
+  // So it's been fetching for a few minutes now,
+  // so that's a bit too much.
+  // But anyway, how do you think we can fix this?
+
+  ////////
+  // So the solution is not to remove this
+  // from the dependency array,
+  // because that is not allowed in React.
+  // Instead, what we need to do is to make this function stable.
+  // So we need it to not be recreated on each re-render.
+  // And the way we do that, as you hopefully know,
+  // is by using the useCallback hook.
   useEffect(
     function () {
       getCity(id);
     },
-    [id]
+
+    [id, getCity]
   );
 
+  /////////////////////////
   const { cityName, emoji, date, notes } = currentCity;
 
   if (isLoading) return <Spinner />;
--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,10 @@
-import { createContext, useContext, useEffect, useReducer } from "react";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useReducer,
+} from "react";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -79,33 +85,110 @@ function CitiesProvider({ children }) {
     }
   }, []);
 
-  async function getCity(id) {
-    //     While the ID that we have right here is a string.
-    // And the reason for that is that we are actually
-    // reading this ID somewhere in that component from the URL.
-    // And so everything that's coming
-    // from the URL will automatically be a string.
-    // And so if you want to do any comparisons like this
-    // then you always need to convert this to a number.
+  ////////////////
+  //   But first of all, of course, we need to understand
+  // what is actually happening.
+  // So now that we have this getCity function
+  // in our dependency array, the effect will rerun
+  // each time that it get city function gets updated,
+  // or in other words, that it gets recreated.
+  // Now, when does this function get recreated?
+  // Well, since it lives in the context, so let's open that.
+  // So this file, getCity, lives here,
+  // so it is created in this city's provider.
+  // But the problem is that this getCity function
+  // will update the state each time that it is executed,
+  // which will then end up in an infinite loop.
+  // So here we call the getCity function, right?
+  // And so that function will then update the state
+  // in this component.
+  // Then this component will re-render,
+  // which will then recreate this function.
+  // And as the function gets recreated,
+  // since it is here in the dependency array,
+  // then getCity will get called again,
+  // which then again will update a state, which will re-render,
+  // which will cause the effect to run over,
+  // and over, and over again.
+  // So that's the infinite loop that we have right here,
+  // and probably we should come back here
+  // and now take our application out of this misery.
+  // So it's been fetching for a few minutes now,
+  // so that's a bit too much.
+  // But anyway, how do you think we can fix this?
 
-    //     So in order to load the city
-    // it of course needs to load from the API,
-    // but if we then click the same again, it doesn't do that.
-    if (currentCity.id === Number(id)) return;
+  ////////
+  // So the solution is not to remove this
+  // from the dependency array,
+  // because that is not allowed in React.
+  // Instead, what we need to do is to make this function stable.
+  // So we need it to not be recreated on each re-render.
+  // And the way we do that, as you hopefully know,
+  // is by using the useCallback hook.
 
-    dispatch({ type: "loading" });
-    try {
-      const res = await fetch(`${BASE_URL}/cities/${id}`);
-      const data = await res.json();
+  const getCity = useCallback(
+    async function getCity(id) {
+      //     While the ID that we have right here is a string.
+      // And the reason for that is that we are actually
+      // reading this ID somewhere in that component from the URL.
+      // And so everything that's coming
+      // from the URL will automatically be a string.
+      // And so if you want to do any comparisons like this
+      // then you always need to convert this to a number.
 
-      dispatch({ type: "city/loaded", payload: data });
-    } catch {
-      dispatch({
-        type: "rejected",
-        payload: "There was an error loading city...",
-      });
-    }
-  }
+      //     So in order to load the city
+      // it of course needs to load from the API,
+      // but if we then click the same again, it doesn't do that.
+      if (currentCity.id === Number(id)) return;
+
+      dispatch({ type: "loading" });
+      try {
+        const res = await fetch(`${BASE_URL}/cities/${id}`);
+        const data = await res.json();
+
+        dispatch({ type: "city/loaded", payload: data });
+      } catch {
+        dispatch({
+          type: "rejected",
+          payload: "There was an error loading city...",
+        });
+      }
+    },
+    [currentCity.id]
+    //     just having this kind of warning here
+    // is reason enough to really always install eslint
+    // in your projects.
+    // So without it, you'll create so many bugs,
+    // it's not gonna be funny, really.
+    // But anyway, this now shows you a real world use case
+    // of this hook, so of this useCallback hook right here.
+    // So this is one of these real world situations
+    // in which you will really need to reach for this tool.
+    // So that's why it's really important
+    // that you paid good attention throughout this section.
+    // And actually remember that when I first introduced
+    // these hooks, this was exactly one of the three use cases
+    // that I talked about.
+    // So memorizing values that are used in the dependency array
+    // of another hook in order to prevent infinite loops.
+
+    ///////////////////So let's give it a reload here,
+
+    // and then let's just try it again.
+    // And if we come here to our network tab,
+    // then we see that there is no problem anymore.
+    // And also here we are not seeing
+    // these infinite requests coming in.
+    // All right, and this is actually
+    // all that I wanted to show you in this lecture.
+    // And I can understand that it probably
+    // seems pretty intimidating,
+    // for example, to find out by yourself
+    // that this would've been the solution here.
+    // But trust me, that with practice and with study,
+    // you will be able to really understand
+    // and to master all these tools.
+  );
 
   async function createCity(newCity) {
     dispatch({ type: "loading" });
@@ -147,6 +230,23 @@ function CitiesProvider({ children }) {
 
   return (
     <CitiesContext.Provider
+      /////////////////////////////////////
+      // But yeah, so again, there's not a lot
+      // to optimize here in this context.
+      // So that's because we don't have any performance issues,
+      // and also because the way that we set up our context value.
+      // So remember how earlier we decided that we want to pass in
+      // all of these functions into the context value,
+      // and so therefore it wouldn't be practical
+      // to create one separate context for each of them, right?
+      // So that's one of the strategies that I mentioned earlier,
+      // but that's not really practical and also not necessary here.
+      // Also, there's not really a need to memorize
+      // this value right here, because we don't have any component
+      // above this provider in the component tree
+      // that might re-render this one.
+      // And so there's no point in doing that,
+      // and so I think that there's actually nothing to do here,
       value={{
         cities,
         isLoading,
